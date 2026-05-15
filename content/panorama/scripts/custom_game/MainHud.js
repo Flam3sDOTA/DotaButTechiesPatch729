c6aa2fdc2613f40436b074481628b88f0af49cea
Flam3s <30335579+Flam3sDOTA@users.noreply.github.com>
2026-05-15 03:23:06 +0300

    Update MainHud.js

--- content/panorama/scripts/custom_game/MainHud.js
+++ content/panorama/scripts/custom_game/MainHud.js
@@ -1,5 +1,4 @@
 "use strict";
-HidePickScreen()
 
 function OnRequestDetonateSelected(data) {
     if (data.target_pid !== Players.GetLocalPlayer()) return;
@@ -12,19 +11,6 @@ function OnRequestDetonateSelected(data) {
     GameEvents.SendCustomGameEventToServer("detonate_selected_mines", {
         entities: selection
     });
-}
-
-
-function HidePickScreen() {
-    if (!Game.GameStateIs(DOTA_GameState.DOTA_GAMERULES_STATE_HERO_SELECTION))
-    {
-        PreGame.style.opacity = "0";
-        $.Schedule(0.25, HidePickScreen);
-    }
-    else
-    {
-        PreGame.style.opacity = "1";
-    }
 }
 
 (function() {
