cd2a3d7ec8ea7f0b30e5b23cf2477d6c56ddf0a4
Flam3s <30335579+Flam3sDOTA@users.noreply.github.com>
2026-05-17 07:45:53 +0300

    Update custom_techies_remote_mines.lua

--- game/scripts/vscripts/abilities/custom_techies_remote_mines.lua
+++ game/scripts/vscripts/abilities/custom_techies_remote_mines.lua
@@ -1,37 +1,9 @@
 custom_techies_remote_mines = class({})
 
 LinkLuaModifier("modifier_custom_techies_remote_mine", "modifiers/modifier_custom_techies_remote_mine", LUA_MODIFIER_MOTION_NONE)
-custom_techies_remote_mines.mine_prop = nil
 
 function custom_techies_remote_mines:GetAOERadius()
     return self:GetSpecialValueFor("radius")
-end
-
-function custom_techies_remote_mines:GetCastAnimation()
-    return ACT_DOTA_CAST_ABILITY_6
-end
-
-function custom_techies_remote_mines:OnAbilityPhaseStart()
-    if not IsServer() then return true end
-    
-    local caster = self:GetCaster()
-    self.mine_prop = SpawnEntityFromTableSynchronous("prop_dynamic", { model = "models/heroes/techies/fx_techies_remotebomb.vmdl", })
-    
-    if self.mine_prop then
-        self.mine_prop:SetParent(caster, "Thumb_1_R")
-        self.mine_prop:SetLocalOrigin(Vector(0, 0, 0))
-        self.mine_prop:SetLocalAngles(0, 0, 0)
-    end  
-    return true 
-end
-
-function custom_techies_remote_mines:OnAbilityPhaseInterrupted()
-    if not IsServer() then return end
-    
-    if self.mine_prop and not self.mine_prop:IsNull() then
-        self.mine_prop:Destroy()
-        self.mine_prop = nil
-    end
 end
 
 function custom_techies_remote_mines:OnSpellStart()
@@ -39,17 +11,10 @@ function custom_techies_remote_mines:OnSpellStart()
     local point  = self:GetCursorPosition()
     if not caster or not point then return end
     
-    if self.mine_prop and not self.mine_prop:IsNull() then
-        self.mine_prop:Destroy()
-        self.mine_prop = nil
-    end
-    
     local mine = CreateUnitByName("npc_dota_techies_custom_remote_mine", point, true, caster, caster, caster:GetTeamNumber())
     mine:SetControllableByPlayer(caster:GetPlayerID(), true)
-    
     local remote_modifier = mine:AddNewModifier(caster, self, "modifier_custom_techies_remote_mine", {})
     mine:AddNewModifier(caster, self, "modifier_kill", {duration = self:GetSpecialValueFor("duration")})
-    
     local talent = caster:FindAbilityByName("special_bonus_unique_techies_25_l")
     if talent and talent:GetLevel() > 0 then
         local bonus_speed = talent:GetSpecialValueFor("value")
@@ -58,7 +23,6 @@ function custom_techies_remote_mines:OnSpellStart()
             remote_modifier:SetRooted(false)
         end
     end
-    
     caster:EmitSound("Hero_Techies.StickyBomb.Plant") 
 end
 
