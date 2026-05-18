d1fe1c3ad453251716f92e565487ca47165bc5e6
Flam3s <30335579+Flam3sDOTA@users.noreply.github.com>
2026-05-19 01:50:16 +0300

    Effect for Mine + Talent

--- game/scripts/vscripts/modifiers/modifier_custom_techies_sign_truesight.lua
+++ game/scripts/vscripts/modifiers/modifier_custom_techies_sign_truesight.lua
@@ -3,6 +3,21 @@ modifier_custom_techies_sign_truesight = class({})
 function modifier_custom_techies_sign_truesight:IsHidden()   return true  end
 function modifier_custom_techies_sign_truesight:IsPurgable()  return false end
 
+function modifier_custom_techies_sign_truesight:OnCreated()
+    if not IsServer() then return end
+    local caster = self:GetCaster()
+    local player = caster:GetPlayerOwner()
+    if player then
+        local fx = ParticleManager:CreateParticleForPlayer(
+            "particles/econ/events/ti10/aghanim_aura_ti10/agh_aura_pre_ti10.vpcf",
+            PATTACH_ABSORIGIN_FOLLOW,
+            self:GetParent(),
+            player
+        )
+        self:AddParticle(fx, true, false, -1, false, false)
+    end
+end
+
 local MINE_UNITS = {
     ["npc_dota_techies_land_mine_custom"]   = true,
     ["npc_dota_techies_stasis_trap"]        = true,
